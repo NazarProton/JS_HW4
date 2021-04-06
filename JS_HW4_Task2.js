c06f7cf4e76c580aa875bee2dda02c05cdff698c
NP <nproton2012@gmail.com>
2021-04-06 03:59:21 +0300

    Nazar_Proton

--- JS_HW4_Task2.js
+++ JS_HW4_Task2.js
@@ -1,30 +1,35 @@
 // Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
 const inventory = {
     items : ['Knife', 'Gas mask'],
-    add(itemName) {
-      console.log(`Adding ${itemName} to inventory`);
+    propertyName : ['Cut','CleaAir'],
+    add(itemName , property) {
+      console.log(`Adding ${itemName} to inventory , with property ${property}`);
       this.items.push(itemName);
+      this.propertyName.push(property);
     },
-    remove(itemName) {
-      console.log(`Removing ${itemName} from inventory`);
+    remove(itemName , property) {
+      console.log(`Removing ${itemName} with propety ${property} from inventory`);
       this.items = this.items.filter(item => item !== itemName);
+      this.propertyName = this.propertyName.filter(prop => prop !== property);
     },
   };
-  const invokeInventoryAction = function(itemName, action , ) {
-    console.log(`Invoking action on ${itemName}`);
-    action.bind(inventory)(itemName);
+  const invokeInventoryAction = function(itemName, action , property , ) {
+    console.log(`Invoking action on ${itemName} with ${property}`);
+    action.bind(inventory)(itemName , property);
   };
-  invokeInventoryAction('Medkit', inventory.add);
+  invokeInventoryAction('Medkit', inventory.add , 'helthUp' , inventory.add);
   // Invoking action on Medkit
   // Adding Medkit to inventory
   console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-  invokeInventoryAction('Gas mask', inventory.remove);
+  invokeInventoryAction('Gas mask', inventory.remove , 'CleaAir' , inventory.remove);
   // Invoking action on Gas mask
   // Removing Gas mask from inventory
   console.log(inventory.items); // ['Knife', 'Medkit']
-  inventory.add("KAR-98")
-  console.log(inventory.items)
-  inventory.remove("Knife")
-  console.log(inventory.items)
-  inventory.add("Spiritus")
-  console.log(inventory.items)
+  inventory.add("KAR-98" , "1 shot 1 hit")
+  console.log(inventory.items , inventory.propertyName)
+  inventory.remove("Knife" , "Cut")
+  console.log(inventory.items , inventory.propertyName)
+  inventory.add("Spiritus" , "drink to relax")
+  console.log(inventory.items , inventory.propertyName)
+  
+  
